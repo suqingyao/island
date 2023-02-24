e2160ad0e00a6c3d7f4528f2b58ef30edb381894
cullyfung <cullyfung@gmail.com>
2023-02-24 22:50:23 +0800

    feat: test

---
+++ scripts/prepare-e2e.ts
@@ -0,0 +1,34 @@
+import path from 'path';
+import fs from 'fs-extra';
+import { execa } from 'execa';
+
+const exampleDir = path.join(__dirname, '../e2e/playground/basic');
+
+const ROOT = path.join(__dirname, '..');
+
+const defaultOptions = {
+  stdout: process.stdout,
+  stdin: process.stdin,
+  stderr: process.stderr
+};
+
+async function prepareE2E() {
+  if (!fs.existsSync(path.resolve(__dirname, '../dist'))) {
+    execa.commandSync('pnpm build', {
+      cwd: ROOT,
+      ...defaultOptions
+    });
+  }
+
+  execa.commandSync('npx playwright install', {
+    cwd: ROOT,
+    ...defaultOptions
+  });
+
+  execa.commandSync('pnpm dev', {
+    cwd: exampleDir,
+    ...defaultOptions
+  });
+}
+
+prepareE2E();
