5c2155c9c7858f73a8e2faaf5127a66634f0782a
cullyfung <cullyfung@gmail.com>
2023-03-04 17:10:04 +0800

    feat: hmr

--- src/node/plugin-mdx/index.ts
+++ src/node/plugin-mdx/index.ts
@@ -1,6 +1,7 @@
 import { Plugin } from 'vite';
+import { pluginMdxHMR } from './pluginMdxHMR';
 import { pluginMdxRollup } from './pluginMdxRollup';
 
 export async function pluginMdx(): Promise<Plugin[]> {
-  return [await pluginMdxRollup()];
+  return [await pluginMdxRollup(), pluginMdxHMR()];
 }
---
+++ src/node/plugin-mdx/pluginMdxHmr.ts
@@ -0,0 +1,34 @@
+import assert from 'assert';
+import { Plugin } from 'vite';
+
+export function pluginMdxHMR(): Plugin {
+  let viteReactPlugin: Plugin;
+  return {
+    name: 'vite-plugin-mdx-hmr',
+    apply: 'serve',
+    configResolved(config) {
+      viteReactPlugin = config.plugins.find(
+        (plugin) => plugin.name === 'vite:react-babel'
+      ) as Plugin;
+    },
+    async transform(code, id, opts) {
+      if (/\.mdx?$/.test(id)) {
+        assert(typeof viteReactPlugin.transform === 'function');
+        const result = await viteReactPlugin.transform.call(
+          this,
+          code,
+          id + '?.jsx',
+          opts
+        );
+        if (
+          result &&
+          typeof result === 'object' &&
+          !result?.code.includes('import.meta.hot.accept(')
+        ) {
+          result.code += 'import.meta.hot.accept();';
+        }
+        return result;
+      }
+    }
+  };
+}
